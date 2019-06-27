9dca229b25a1aa48d02035c52c99df790f851643
Pickling Cristina <pcristin@il-i4.21-school.ru>
2019-06-27 17:28:46 +0300

    New header and makefile

--- oficial/hdr/libft.h
+++ oficial/hdr/libft.h
@@ -6,13 +6,14 @@
 /*   By: pcristin <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/05 18:35:49 by pcristin          #+#    #+#             */
-/*   Updated: 2019/05/13 09:22:13 by pcristin         ###   ########.fr       */
+/*   Updated: 2019/06/27 17:18:55 by pcristin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFT_H
 # define LIBFT_H
 # include <stdlib.h>
+# include <unistd.h>
 
 void			*ft_memalloc(size_t size);
 void			ft_memdel(void **ap);
@@ -31,6 +32,13 @@ char			*ft_strjoin(char const *s1, char const *s2);
 char			*ft_strsub(char const *s, unsigned int start, size_t len);
 unsigned int	ft_whcount(char const *s);
 char			*ft_strtrim(char const *s);
-unsigned int	delimc(char const *s, char c);
 char			**ft_strsplit(char const *s, char c);
+void			ft_putchar(char c);
+void			ft_putstr(char const *s);
+void			ft_putendl(char const *s);
+void			ft_putnbr(int n);
+void			ft_putchar_fd(char c, int fd);
+void			ft_putstr_fd(char const *s, int fd);
+void			ft_putendl_fd(char const *s, int fd);
+void			ft_putnbr_fd(int n, int fd);
 #endif
