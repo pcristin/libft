107cf0b8af441d1add45ac73e658b7d0b6158961
Pickling Cristina <pcristin@oa-k2.21-school.ru>
2019-06-29 04:34:53 +0300

    Version 1.1

--- oficial/hdr/libft.h
+++ oficial/hdr/libft.h
@@ -6,7 +6,7 @@
 /*   By: pcristin <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/05 18:35:49 by pcristin          #+#    #+#             */
-/*   Updated: 2019/06/28 03:12:37 by pcristin         ###   ########.fr       */
+/*   Updated: 2019/06/29 04:29:08 by pcristin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,6 +41,32 @@ void			ft_putchar_fd(char c, int fd);
 void			ft_putstr_fd(char const *s, int fd);
 void			ft_putendl_fd(char const *s, int fd);
 void			ft_putnbr_fd(int n, int fd);
-char            *ft_strcpy(char *s1, char const *s2);
-char		    *ft_itoa(int n);
+char			*ft_strcpy(char *s1, char const *s2);
+char			*ft_itoa(int n);
+void			*ft_memset(void	*b, int c, size_t len);
+void			ft_bzero(void *s, size_t n);
+void			*ft_memcpy(void *dst, const void *src, size_t n);
+void			*ft_memccpy(void *dst, const void *src, int c, size_t n);
+void			*ft_memmove(void *dst, const void *src, size_t len);
+void			*ft_memchr(const void *s, int c, size_t n);
+int				ft_memcmp(const void *s1, const void *s2, size_t n);
+char			*ft_strdup(char const *s1);
+char			*ft_strncpy(char *dest, const char *src, size_t len);
+char			*ft_strcat(char *s1, const char *s2);
+char			*ft_strncat(char *s1, const char *s2, size_t n);
+size_t			ft_strlcat(char *dst, const char *src, size_t size);
+char			*ft_strchr(const char *s, int c);
+char			*ft_strrchr(const char *s, int c);
+char			*ft_strstr(char *haystack, char *needle);
+char			*ft_strnstr(const char *haystack, const char *needle, \
+																	size_t len);
+int				ft_isalpha(int c);
+int				ft_islower(int c);
+int				ft_isupper(int c);
+int				ft_isascii(int c);
+int				ft_isdigit(int c);
+int				ft_toupper(int c);
+int				ft_tolower(int c);
+int				ft_isalnum(int c);
+int				ft_isprint(int c);
 #endif
